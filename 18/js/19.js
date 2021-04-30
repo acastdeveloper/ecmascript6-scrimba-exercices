d77a733bf00d3526fadd50a7e0a5402ea2761f2f
Àlex Castelló <ac.desenvolupador@gmail.com>
2021-04-30 13:58:05 +0200

    Ecma Script exercicies of Scrimba

---
+++ 18/js/19.js
@@ -0,0 +1,5 @@
+import { add } from './test19.js';
+
+let result = add(3,2);
+
+console.log(result);
---
+++ js/22.js
@@ -0,0 +1,31 @@
+class Player {
+    constructor(name,country) {
+        this.name = name;
+        this.country = country;
+    }
+
+    introduce() {
+console.log(`${this.name} was born in ${this.country}`);
+    }
+}
+
+
+const messi = new Player("Messi", "Argentina");
+
+messi.introduce();
+
+class TennisPlayer extends Player {
+    constructor(name, country, age) {
+        super(name, country);
+        this.age=age;
+    }
+
+    playTennis() {
+        console.log(`${this.name} is ${this.age} years old and know how to play Tennis`);
+    }
+}
+
+const roger = new TennisPlayer('Roger Federer','Switzerland','38');
+
+roger.introduce();
+roger.playTennis();
